161d0927c718933abb450f13f11b6599d8440e02
余杰 <yujie@ahsoft.com>
2019-02-13 08:50:47 +0800

    更新整个项目

--- src/com/github/yu_jay/fileChangeListenInEclipse/App.java
+++ src/com/github/yu_jay/fileChangeListenInEclipse/App.java
@@ -1,37 +1,12 @@
 package com.github.yu_jay.fileChangeListenInEclipse;
 
-import java.nio.charset.Charset;
-
-import org.apache.log4j.Logger;
 import org.eclipse.ui.IStartup;
 
-import com.github.yu_jay.jay_common.act.Log4jLogger;
-import com.github.yu_jay.jay_common.iter.ILogger;
-
-/**
- * 插件入口文件
- * @author yujie
- *
- */
 public class App implements IStartup {
-	
-	private Logger log = Logger.getLogger(App.class);
-	
-	static {
-		String logPath = "G:/Template/log4j.properties";
-        ILogger logger = new Log4jLogger(logPath);
-	}
 
 	@Override
 	public void earlyStartup() {
 		
-		
-		
-		String code = Charset.defaultCharset().name();
-		System.out.println("----code---: " + code);
-		
-		System.out.println("流浪地球");
-		log.debug("流浪地球。");
 	}
 
 }
